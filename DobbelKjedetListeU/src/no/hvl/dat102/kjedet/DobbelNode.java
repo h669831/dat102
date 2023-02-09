f0be879825275ebea3aa0307d30081c420e972ad
Kronstad <Harald.Soleim@hib.no>
2023-02-09 11:32:37 +0100

    v01

--- DobbelKjedetListeU/src/no/hvl/dat102/kjedet/DobbelNode.java
+++ DobbelKjedetListeU/src/no/hvl/dat102/kjedet/DobbelNode.java
@@ -1,80 +1,77 @@
 package no.hvl.dat102.kjedet;
 
 //********************************************************************
+
 //  DobbelNode.java       
 //
 //  Representer en node i en dobbelkjedet liste..
 //********************************************************************
 
+public class DobbelNode<T> {
+	private DobbelNode<T> neste;
+	private DobbelNode<T> forrige;
+	private T element;
 
-public class DobbelNode<T>{
-    private DobbelNode<T> neste;
-    private DobbelNode<T> forrige;
-    private T element;
+	/******************************************************************
+	 * Oppretter en tom node node.
+	 ******************************************************************/
+	public DobbelNode() {
+		neste = null;
+		forrige = null;
+		element = null;
 
-    /******************************************************************
-      Oppretter en tom node node.
-    ******************************************************************/
-    public DobbelNode(){    
-       neste   = null;
-       element = null;
-       forrige = null;
-    }
+	}
 
-    /******************************************************************
-      Oppretter en node som lagrer det spesifiserte elementet.
-    ******************************************************************/
-    public DobbelNode(T elem) {
-       neste = null;
-       forrige = null;
-       element = elem;
-    }
+	/******************************************************************
+	 * Oppretter en node som lagrer det spesifiserte elementet.
+	 ******************************************************************/
+	public DobbelNode(T elem) {
+		neste = null;
+		forrige = null;
+		element = elem;
+	}
 
-    /******************************************************************
-      Returnerer en referanse til etterfølgeren av denne noden, 
-      eller null hvis denne er siste.
-    ******************************************************************/
-    public DobbelNode<T> getNeste() {
-       return neste;
-    }
+	/******************************************************************
+	 * Returnerer en referanse til etterfølgeren av denne noden, eller null hvis
+	 * denne er siste.
+	 ******************************************************************/
+	public DobbelNode<T> getNeste() {
+		return neste;
+	}
 
-    /*************************************************************************
-      Returnerer forgjengeren til denne noden, eller null hvis denne er første 
-    **************************************************************************/
-    public DobbelNode<T>  getForrige() {
-       return forrige;
-    }
+	/*************************************************************************
+	 * Returnerer forgjengeren til denne noden, eller null hvis denne er første
+	 **************************************************************************/
+	public DobbelNode<T> getForrige() {
+		return forrige;
+	}
 
-    /******************************************************************
-      Setter neste til å peke på dnode.
-    ******************************************************************/
-    public void setNeste (DobbelNode<T> dnode){
-       neste = dnode;
-    }
+	/******************************************************************
+	 * Setter neste til å peke på dnode.
+	 ******************************************************************/
+	public void setNeste(DobbelNode<T> dnode) {
+		neste = dnode;
+	}
 
-    /******************************************************************
-      Settter ny forrige til å peke på dnode.
-    ******************************************************************/
-    public void setForrige(DobbelNode<T> dnode) {
-       forrige = dnode;
-    }
+	/******************************************************************
+	 * Settter ny forrige til å peke på dnode.
+	 ******************************************************************/
+	public void setForrige(DobbelNode<T> dnode) {
+		forrige = dnode;
+	}
 
+	/******************************************************************
+	 * Returnerer elementet til denne noden..
+	 ******************************************************************/
+	public T getElement() {
+		return element;
+	}
 
-    /******************************************************************
-      Returnerer elementet til denne noden..
-    ******************************************************************/
-    public T getElement() {
-       return element;
-    }
-
-    /******************************************************************
-      Lagrer nytt element i denne noden..
-    ******************************************************************/
-    public void setElement (T elem) {
-       element = elem;
-    }
+	/******************************************************************
+	 * Lagrer nytt element i denne noden..
+	 ******************************************************************/
+	public void setElement(T elem) {
+		element = elem;
+	}
 
 }
-
-
-
